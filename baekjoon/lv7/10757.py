838ff789e40589c7dac8afafe22058229a3409c1
Harinee68 <harin608@naver.com>
2022-09-15 14:32:53 +0900

    [BOJ] Lv7. 기본 수학1 220915
    - 10757: 큰 수 A+B
    - 1712: 손익분기점
    - 2292: 벌집

---
+++ baekjoon/lv7/10757.py
@@ -0,0 +1,6 @@
+# 브론즈5
+# 10757: 큰 수 A+B
+
+A,B = map(int, input().split())
+
+print(A+B)
--- baekjoon/lv7/2292.py
+++ baekjoon/lv7/2292.py
@@ -1,12 +1,24 @@
 # 브론즈2
 # 2292: 벌집
 
-N = int(input()) # N 입력 받음
-bee_house = 1 # 벌집 개수, 처음은 1
-cnt = 1 # 층 개수, 처음은 1
+# 220915 풀이
+N = int(input())
+bee_house = 1 
+cnt = 1
 
-while N>bee_house: #벌집 개수가 N보다 작을 때 반복문 실행
+while N>bee_house: 
     bee_house += cnt*6
     cnt+=1
 
 print(cnt)
+
+# # 220906 풀이
+# N = int(input()) # N 입력 받음
+# bee_house = 1 # 벌집 개수, 처음은 1
+# cnt = 1 # 층 개수, 처음은 1
+
+# while N>bee_house: #벌집 개수가 N보다 작을 때 반복문 실행
+#     bee_house += cnt*6
+#     cnt+=1
+
+# print(cnt)
