936ef5fb9efa6b2224ff5306d07fde5a364a3d91
Harinee68 <harin608@naver.com>
2022-09-16 17:07:05 +0900

    [BOJ] Lv7. 기본 수학1 220916
    1193: 분수 찾기
    2869: 달팽이는 올라가고 싶다

--- baekjoon/lv7/1193.py
+++ baekjoon/lv7/1193.py
@@ -1,28 +1,47 @@
 # 브론즈 1
 # 1193: 분수찾기
 
-# 검색 후 수정한 코드
-input_num = int(input()) # 찾을 분수의 index 입력
+# 220916 직접 구현한 코드
+X = int(input())
+line = 1
 
-line=0 # line 선언
-end_index = 0 # 라인의 젤 끝에있는 인덱스
-
-while input_num > end_index: #input_num이 end_index보다 클 때 계속 반복
+while True:
+    X-=line
+    if X<=0:
+        break
     line+=1
-    end_index+=line #1라인:1, 2라인:3, 3라인:6 가 각각 라인의 제일 큰, 제일 끝인 인덱스값이다.
+X+=line
 
-# line이 짝수일 때는 [1/2, 2/1] 처럼 분자는 증가하고 분모는 감소한다.
-# line이 홀수 일 때는 [3/1, 2/2, 1/3] 처럼 분자는 감소하고 분모는 증가한다.
-
-diff = end_index - input_num #end_index와 input_num의 차이 계산.
 if line%2==0:
-    top = line-diff
-    bottom = diff+1
+    top = X
+    bottom = line-X+1
 else:
-    top = diff+1
-    bottom = line-diff
-
+    top = line-X+1
+    bottom = X
 print(f"{top}/{bottom}")
+
+# # 검색 후 수정한 코드
+# input_num = int(input()) # 찾을 분수의 index 입력
+
+# line=0 # line 선언
+# end_index = 0 # 라인의 젤 끝에있는 인덱스
+
+# while input_num > end_index: #input_num이 end_index보다 클 때 계속 반복
+#     line+=1
+#     end_index+=line #1라인:1, 2라인:3, 3라인:6 가 각각 라인의 제일 큰, 제일 끝인 인덱스값이다.
+
+# # line이 짝수일 때는 [1/2, 2/1] 처럼 분자는 증가하고 분모는 감소한다.
+# # line이 홀수 일 때는 [3/1, 2/2, 1/3] 처럼 분자는 감소하고 분모는 증가한다.
+
+# diff = end_index - input_num #end_index와 input_num의 차이 계산.
+# if line%2==0:
+#     top = line-diff
+#     bottom = diff+1
+# else:
+#     top = diff+1
+#     bottom = line-diff
+
+# print(f"{top}/{bottom}")
 
 
 
--- baekjoon/lv7/2869.py
+++ baekjoon/lv7/2869.py
@@ -1,14 +1,25 @@
 # 실버5
 # 2869: 달팽이는 올라가고 싶다.
 
+# 9월 16일 푼 방식
+
+import math
 A,B,V = map(int, input().split())
+X = (V-A)/(A-B)
+# X(A-B) + A >= V 에서 생각해낸 식
+# A가 더해진다는 건 하루가 더 흘렀다는 이야기이기 때문에
+# X값에 1을 더한 상태로 올림을 해준다.
+print(math.ceil(X+1))
 
-# a*days - b*(days-1) >= V : 낮시간
-# a*days - b*days - b >= V
-# days(a-b) >= V+b
-# days >= (V+b)/(a-b)
+# #  9월 8일 푼 방식
+# A,B,V = map(int, input().split())
 
-if (V-B) % (A-B) == 0:
-    print((V-B)//(A-B))
-else:
-    print((V-B)//(A-B)+1)
+# # a*days - b*(days-1) >= V : 낮시간
+# # a*days - b*days - b >= V
+# # days(a-b) >= V+b
+# # days >= (V+b)/(a-b)
+
+# if (V-B) % (A-B) == 0:
+#     print((V-B)//(A-B))
+# else:
+#     print((V-B)//(A-B)+1)
